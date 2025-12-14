86528c27510d1893b638d844cc6a84bf33ee3080
JorikFat <jorikfat@gmail.com>
2025-12-14 14:25:04 +0300

    add: excercises layout

---
+++ lib/excercises_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+final excercisesStub = [
+  "Отжимания",
+  "Подтягивания",
+  "Приседания",
+];
+
+class ExcercisesScreen extends StatelessWidget {
+  const ExcercisesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(
+        itemCount: excercisesStub.length,
+        separatorBuilder: (_, __) => const SizedBox(height: 16),
+        itemBuilder: (context, index) => Card(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text(excercisesStub[index]),
+          ),
+        ),
+      ),
+    );
+  }
+}
