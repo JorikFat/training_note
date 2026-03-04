eab1de602a47589f3d3af378fb285d739f2a6baa
Badrakova Valeria <lera_18a@mail.ru>
2026-03-04 15:38:01 +0300

    feat(view_model): added TrainingsScreenViewModelwith create/delete implementation

---
+++ lib/ui/training/view_model/trainings_screen_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:training_note/domain/models/approach.dart';
+import 'package:training_note/domain/models/training.dart';
+
+class TrainingsScreenViewModel extends ValueNotifier<List<Training>> {
+  TrainingsScreenViewModel(List<Training>? trainings) : super(trainings ?? []);
+
+  void createTrainigs(DateTime date, List<Approach> approach) {
+    final newTraining =
+        Training(date: date, approach: approach, id: DateTime.now().second);
+    value = [...value, newTraining];
+  }
+
+  // void updateTraining(DateTime date, List<Approach> approach, int id) {
+  //   final index = value.indexWhere((e) => e.id == id);
+  //.  approach еще обновить
+  //   final updated = value[index].copyWith(date: date, approach: approach);
+  //   final newList = [...value];
+  //   newList[index] = updated;
+  //   value = newList;
+  // }
+
+  void deleteTraining(int id) {
+    value = value.where((e) => e.id != id).toList();
+  }
+}
+
+final TrainingsScreenViewModel trainingsScreenViewModel =
+    TrainingsScreenViewModel(trainings);
