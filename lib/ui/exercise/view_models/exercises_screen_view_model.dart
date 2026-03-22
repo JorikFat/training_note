bafec174f7c635e5a97f8a56fa13cd0821adf16e
Badrakova Valeria <lera_18a@mail.ru>
2026-03-22 19:06:10 +0300

    feat(ui/exercise/view_models): implemented exercises crud with drift

--- lib/ui/exercise/view_models/exercises_screen_view_model.dart
+++ lib/ui/exercise/view_models/exercises_screen_view_model.dart
@@ -1,28 +1,55 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:training_note/data/database.dart';
 import 'package:training_note/domain/models/exercise.dart';
 
 class ExercisesScreenViewModel extends ValueNotifier<List<Exercise>> {
-  ExercisesScreenViewModel(
-    super._value,
-  );
+  ExercisesScreenViewModel(super._value, {required this.database});
+  final AppDatabase database;
 
-  void createExercise(String name) {
-    //value - сеттер оповещает автоматически всех подписчиков об изменении value (стейта)
-    final newExercise =
-        Exercise(name: name, id: DateTime.now().millisecondsSinceEpoch);
-    value = [...value, newExercise];
+  Future<void> load() async {
+    final rows = await database.select(database.exerciseData).get();
+    value = rows.map((e) => Exercise(name: e.name, id: e.id)).toList();
   }
 
-  void updateExercise(int id, String newName) {
+  Future<void> create(String name) async {
+    final insertedRow = await database
+        .into(database.exerciseData)
+        .insertReturning(ExerciseDataCompanion(name: Value(name)));
+    final createdExercise =
+        Exercise(name: insertedRow.name, id: insertedRow.id);
+    value = [...value, createdExercise];
+  }
+
+  // Stream<List<ExerciseDataData>> read() async* {
+  //   database.select(database.exerciseData).watch();
+  //   value = value.((e) => e.name).toList();
+  // }
+
+  //   //value - сеттер оповещает автоматически всех подписчиков об изменении value (стейта)
+
+  Future<void> update(int id, String newName) async {
+    final current = database.update(database.exerciseData);
+    current.where((e) => e.id.equals(id));
+    final updated = await current.write(
+      ExerciseDataCompanion(
+        name: Value(newName),
+      ),
+    );
+    if (updated == 0) return;
     final index = value.indexWhere((e) => e.id == id);
-    // if (index == -1) return;
-    final updated = value[index].copyWith(name: newName);
+    if (index == -1) return;
+    final updatedValue = value[index].copyWith(name: newName);
     final newList = [...value];
-    newList[index] = updated;
+    newList[index] = updatedValue;
     value = newList;
   }
 
-  void deleteExercise(int id) {
+  Future<void> delete(int id) async {
+    final trackedId = database.delete(database.exerciseData);
+    trackedId.where((e) => e.id.equals(id));
+    //удаление
+    await trackedId.go();
     value = value.where((e) => e.id != id).toList();
   }
 }
